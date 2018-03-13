9e721573c27d0c517d94536b87f59c330c046460
SamDino <samuel.cas.96@gmail.com>
2018-03-12 20:38:12 -0600

    Weather API and visual test.

--- flower_object.js
+++ flower_object.js
@@ -1,13 +1,23 @@
-var data = 
-function setup() {
 
+var weather;
+
+function setup() {
+	createCanvas(600, 600);
+	loadJSON('http://api.openweathermap.org/data/2.5/weather?q=Monterrey&appid=15a6280447437dc315cf5b7bb4966a79&units=metric', gotData);
 }
 
-function getData(data) {
-
+function gotData(data) {
+	weather = data;
 }
 
 function draw() {
-	
-	
+	background(0);
+	randomSeed(5);
+	text('Weather in Monterrey', 10, 50);
+	textSize(20);
+	fill(255);
+	if(weather){
+		text('Temperature: ', 100, 150);
+		text(weather.main.temp, 220, 150);
+	}
 }
