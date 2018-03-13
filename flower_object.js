8d8a94c16075b4687f7609ec63a37cc9e103a5dd
SamDino <samuel.cas.96@gmail.com>
2018-03-12 18:36:28 -0600

    How many people is in the sky?

--- flower_object.js
+++ flower_object.js
@@ -1,27 +1,40 @@
-var data;
 
-function preload(){
+var x = 0;
+var spaceData;
 
-	data = loadJSON("birds.json");
+function setup() {
+	createCanvas(500, 500);
+	// url, callback, (optional - to acces some places). 
+	loadJSON("http://api.open-notify.org/astros.json", gotData, 'jsonp');
 
 }
 
-function setup() {
-
-	noCanvas();
-
-	var birds = data.birds;
-	for(var i = 0; i < birds.length; i++){
-		createElement('h1', birds[i].family);
-		var members = birds[i].members;
-		for (var j = 0; j < members.length; j++) {
-			createDiv(members[j]);
-		}
-	}
-
+function gotData(data) {
+	spaceData = data;
 }
 
 function draw() {
+	background(0);
+	if(spaceData){
+		randomSeed(11);
+		for(var i = 0; i < spaceData.number; i++) {
+		var name = spaceData.people[i].name;
+		var defWidth = random(width);
+		var defHeigh = random(height);
+		fill(255);
+		ellipse(defWidth, defHeigh, 16, 16);
+		text(name, defWidth + 16, defHeigh + 16, 60, 60);
+		fill(255);
+		}
+	}
 	
+
+	stroke(255);
+	line(x, 0, x, height);
+	x = x + 1;
+	if(x > width) {
+		x = 0;
+	}
+
 	
 }
